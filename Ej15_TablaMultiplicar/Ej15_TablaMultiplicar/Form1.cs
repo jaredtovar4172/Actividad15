0a93fa50ac9ee1df7cb8516542b6c347345adf2d
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-23 21:12:47 -0600

    Add files via upload

---
+++ Ej15_TablaMultiplicar/Ej15_TablaMultiplicar/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej15_TablaMultiplicar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tabla = int.Parse(textBox1.Text);
+            int limite = int.Parse(textBox2.Text);
+            int resultado;
+            for (int i = 1; i <= limite; i++)
+            {
+                resultado = tabla * i;
+                listBox1.Items.Add(tabla + "x" + i + " = " + resultado);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Focus();
+            textBox2.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
